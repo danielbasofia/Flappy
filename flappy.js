9be016b1a51ada2166f22a157ed212b2d86622c7
Daniel Valverde <devchb@hotmail.com>
2017-10-02 14:11:14 -0500

    Flappy Bird Base

---
+++ flappy.js
@@ -0,0 +1,32 @@
+
+var bird;
+var pipes = [];
+
+function setup(){
+    createCanvas(400, 600);
+    bird = new Bird();
+    pipes.push(new Pipe());
+}
+
+function draw() {
+    background(0);
+    bird.update();
+    bird.show();
+
+    if(frameCount % 80 == 0){
+        pipes.push(new Pipe());
+    }
+
+    for (var i= 0; i < pipes.length ; i++){
+        pipes[i].show();
+        pipes[i].update();
+        
+    }
+}
+
+function keyPressed(){
+    if(key == ' '){
+        bird.up();
+        //console.log('SPACE');
+    }
+}
